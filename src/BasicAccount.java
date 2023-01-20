3d8c8e2b1455b57906eda2597d017027f88e4791
NoamRoseman97 <Noam.roseman@gmail.com>
2023-01-21 00:52:40 +0200

    This is my HomeWork1

--- src/BasicAccount.java
+++ src/BasicAccount.java
@@ -1,48 +1,23 @@
 public class BasicAccount extends StandardAccount{
 
     //State
-    // double Balance;
-    // int AccountNum;
     double WithdrawLim;
 
 
     //Constructor
     public BasicAccount (int accountNumber, double withdrawLimit) {
-        // this.AccountNum = accountNumber;
         super (accountNumber,0);
         this.WithdrawLim = withdrawLimit;
-         //this.Balance = 0;
+
     }
 
-
     //Behaviour
-//    @Override
-//    public void Deposit(double amount) {
-//        this.Balance += amount;
-//    }
-
     @Override
     public double Withdraw(double amount) // method argument
     {
         if (amount > WithdrawLim) {
             amount = WithdrawLim;
         }
-        return super.Withdraw(amount); //In both cases
-//        if (amount > Balance) {
-//            amount = Balance;
-//        }
-//        Balance -= amount;
-//        return amount;
+        return super.Withdraw(amount);
     }
-
-//
-//    @Override
-//    public double GetCurrentBalance() {
-//        return this.Balance;
-//    }
-//
-//    @Override
-//    public int GetAccountNumber() {
-//        return this.AccountNum;
-//    }
 }
--- src/PremiumAccount.java
+++ src/PremiumAccount.java
@@ -1,41 +1,15 @@
 public class PremiumAccount extends StandardAccount{
 
     //State
-    //double Balance;
-    //int AccountNum;
-
-
 
     //Constructor
     public PremiumAccount(int accountNumber) {
         super(accountNumber,Double.NEGATIVE_INFINITY);
         //this.AccountNum = accountNumber;
-        //this.Balance = 0;
+
     }
 
-
     // Behaviour
-
-//    @Override
-//    public void Deposit(double amount) {
-//        Balance += amount;
-//    }
-//
-//    @Override
-//    public double Withdraw(double amount) {
-//        Balance -= amount;
-//        return amount;
-//        }
-
-//    @Override
-//    public double GetCurrentBalance() {
-//        return Balance;
-//    }
-//
-//    @Override
-//    public int GetAccountNumber() {
-//        return AccountNum;
-//    }
 }
 
 
